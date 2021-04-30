c5194fb588d6a052b40e8f452ed921e98fdeebaf
Nurian Coelho <nuriancoelho@hotmail.com>
2021-04-30 20:59:34 -0300

    Novos ícones

--- src/assets/icons/User/index.tsx
+++ src/assets/icons/User/index.tsx
@@ -8,8 +8,7 @@ interface Props {
 }
 
 export const UserIcon: React.FC<Props> = ({ width, height, color }) => (
-    <Svg width={width} height={height} viewBox={`0 0 ${width} ${height}`} fill="none" stroke={color} strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-        <Path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2" />
-        <Circle cx="12" cy="7" r="4" />
+    <Svg width={width} height={height} viewBox="0 0 448 512">
+        <Path fill={color} d="M224 256c70.7 0 128-57.3 128-128S294.7 0 224 0 96 57.3 96 128s57.3 128 128 128zm89.6 32h-16.7c-22.2 10.2-46.9 16-72.9 16s-50.6-5.8-72.9-16h-16.7C60.2 288 0 348.2 0 422.4V464c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48v-41.6c0-74.2-60.2-134.4-134.4-134.4z" />
     </Svg>
 );
--- src/assets/icons/index.tsx
+++ src/assets/icons/index.tsx
@@ -1,13 +1,23 @@
+import { CompanyIcon } from "./Company"
 import { HomeIcon } from "./Home"
+import { KeyIcon } from "./Key"
+import { LinkIcon } from "./Link"
 import { LogoIcon } from "./Logo"
+import { PhoneIcon } from "./Phone"
+import { PinIcon } from "./Pin"
 import { PlusIcon } from "./Plus"
 import { TrashIcon } from "./Trash"
 import { UserIcon } from "./User"
 
 
 export {
+    CompanyIcon,
     HomeIcon,
+    KeyIcon,
+    LinkIcon,
     LogoIcon,
+    PhoneIcon,
+    PinIcon,
     PlusIcon,
     TrashIcon,
     UserIcon,
