6241abc0b552dccf8a7c0a1643bb605b3bbc50ea
Nurian Coelho <nuriancoelho@hotmail.com>
2021-05-01 12:11:59 -0300

    Tela para criar novo post

--- src/pages/PlusPost/index.tsx
+++ src/pages/PlusPost/index.tsx
@@ -1,14 +1,88 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { useState } from 'react';
 import { LinearGradient } from 'expo-linear-gradient';
+import { Platform, Keyboard } from 'react-native';
+
+import {
+    Container,
+    Box,
+    Form,
+    Label,
+    Input,
+    TextArea,
+    Button,
+    TextButton,
+    Header,
+    Title
+} from './styles';
+import { setPost } from '../../services';
+import { PropsPost } from '../../services/@types/Posts';
 
 export const PlusPost: React.FC = () => {
+    const [dataForm , setDataForm] = useState<PropsPost>({
+        userId: undefined,
+        id: undefined,
+        title: '',
+        body: ''
+    });
+    const [send, setSend] = useState<number>();
+
     return (
-        <LinearGradient
-            colors={['rgba(111,52,237,0.34)', 'rgba(10, 88, 255, 0.35)']}
-            style={{ flex: 1 }}
-        >
-            <Text> Plus Post </Text>
-        </LinearGradient>
+        <Container>
+            <LinearGradient
+                colors={['rgba(111,52,237,0.34)', 'rgba(10, 88, 255, 0.35)']}
+                style={{ flex: 1 }}
+            >
+                <Header>
+                    <Title>
+                        Adicione um novo Post
+                    </Title>
+                </Header>
+                <Box>
+                    <Form
+                        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+                    >
+                        <Label>
+                            1. Informe aqui o título do seu post:
+                        </Label>
+                        <Input
+                            placeholder="Título do seu post"
+                            onChangeText={(val) => setDataForm({
+                                ...dataForm,
+                                title: val
+                            })}
+                            onSubmitEditing={Keyboard.dismiss}
+                        />
+
+                        <Label>
+                            2. Informe aqui a descrição do seu post:
+                        </Label>
+                        <TextArea
+                            placeholder="Descrição do seu post"
+                            multiline={true}
+                            textAlignVertical="top"
+                            onChangeText={(val) => setDataForm({
+                                ...dataForm,
+                                body: val
+                            })}
+                            onSubmitEditing={Keyboard.dismiss}
+                        />
+
+                        <Button
+                            onPress={
+                                () => setPost(dataForm).then((response) => {
+                                    console.log(response)
+                                    setSend(response.id)
+                                })
+                            }
+                        >
+                            <TextButton>
+                                Enviar
+                            </TextButton>
+                        </Button>
+                        
+                    </Form>
+                </Box>
+            </LinearGradient>
+        </Container>
     )
 }
---
+++ src/pages/PlusPost/styles.ts
@@ -0,0 +1,88 @@
+import styled from 'styled-components/native';
+import { Dimensions, StatusBar } from 'react-native';
+
+interface PropsCard {
+    isLast: boolean
+}
+
+export const Container = styled.SafeAreaView`
+    flex: 1;
+`;
+
+export const Header = styled.View`
+    padding-top: ${StatusBar.currentHeight && StatusBar.currentHeight + 20}px;
+    height: 120px;
+    padding-bottom: 10px;
+    padding-right: 30px;
+    padding-left: 30px;
+    border-bottom-color: #F5F5F5;
+    border-bottom-width: 1px;
+    justify-content: flex-end;
+`;
+
+export const Title = styled.Text`
+    font-size: 18px;
+    font-family: Poppins_700Bold;
+    text-align: center;
+    color: #000;
+`;
+
+export const Box = styled.ScrollView`
+    flex: 1;
+    padding-top: 70px;
+    padding-right: 20px;
+    padding-left: 20px;
+`;
+
+export const Form = styled.KeyboardAvoidingView`
+    margin-bottom: 100px;
+    flex: 1;
+`;
+
+export const Label = styled.Text`
+    font-size: 16px;
+    margin-bottom: 3px;
+    margin-top: 30px;
+    padding-left: 20px;
+    font-family: Poppins_500Medium;
+`;
+
+export const Input = styled.TextInput`
+    width: ${ Dimensions.get('window').width * 0.89}px;
+    height: 40px;
+    border-radius: 10px;
+    border-color: #0057FF;
+    border-width: 1px;
+    background-color: #FFF;
+    padding-left: 20px;
+    padding-right: 20px;
+`;
+
+export const TextArea = styled.TextInput`
+    width: ${ Dimensions.get('window').width * 0.89}px;
+    height: 100px;
+    border-radius: 10px;
+    border-color: #0057FF;
+    border-width: 1px;
+    background-color: #FFF;
+    padding: 20px;
+    margin-bottom: 50px;
+`;
+
+export const Button = styled.TouchableOpacity`
+    width: ${ Dimensions.get('window').width * 0.3}px;
+    height: 50px;
+    background-color: #0057FF;
+    border-radius: 5px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    align-self: center;
+    margin-bottom: 100px;
+`;
+
+export const TextButton = styled.Text`
+    font-size: 16px;
+    color: #FFF;
+    font-family: Poppins_400Regular;
+`;
