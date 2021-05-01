ecfc804cbe97a71aed45a9e41b8c6de90488b8dc
Nurian Coelho <nuriancoelho@hotmail.com>
2021-05-01 15:48:33 -0300

    Criação do contexto, para acessar o hook de criação e leitura dos posts

--- App.tsx
+++ App.tsx
@@ -10,7 +10,12 @@ import {
 
 import Routes from './src/routes';
 
+import PostsContext from './src/contexts/Posts/index';
+import { usePost } from './src/hooks/usePost';
+
 export default function App() {
+  const { getPost, storePost } = usePost();
+  
   const [fontsLoaded] = useFonts({
     Poppins_400Regular,
     Poppins_500Medium,
@@ -23,7 +28,9 @@ export default function App() {
 
   } else {
     return (
-      <Routes />
+      <PostsContext.Provider value={{ getPost, storePost }}>
+        <Routes />
+      </PostsContext.Provider>
     );
   };
 }
---
+++ src/contexts/Posts/index.ts
@@ -0,0 +1,11 @@
+import { createContext } from 'react';
+import { PropsPost } from '../../services/@types/Posts';
+
+interface PropsUsePost {
+    getPost: () => Promise<Array<PropsPost> | undefined>,
+    storePost: (e: PropsPost) => Promise<void>
+}
+
+const PostsContext = createContext<PropsUsePost>({} as PropsUsePost);
+
+export default PostsContext;
